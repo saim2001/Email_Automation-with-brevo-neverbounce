470d1e9ce21b8f7dba0e622bfd2100de979fdca3
saim2001 <saimrao49@gmail.com>
2023-05-24 05:47:30 +0500

    updated

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
 from __future__ import print_function
-import sib_api_v3_sdk
-from sib_api_v3_sdk.rest import ApiException
-from pprint import pprint
-import neverbounce_sdk
-import pandas as pd
+from utils import *
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
-def load_data(csv_path):
-    # Specify the columns to read from the CSV
-    columns = ['name', 'email']
-    # Read the CSV file and select the desired columns
-    data = pd.read_csv(csv_path, usecols=columns)
-    # Convert the DataFrame to a dictionary
-    data_dict = data.to_dict(orient='list')
-    return data_dict
 
-def configure_neverbounce_client(API_key):
-    api_key = API_key
-    # Configure the NeverBounce client using the provided API key
-    client = neverbounce_sdk.client(api_key=api_key, timeout=30)
-    # Get the account information using the configured client
-    info = client.account_info()
-    pprint(info)
 
-def configure_Brevo_client(API_key):
-    configuration = sib_api_v3_sdk.Configuration()
-    configuration.api_key['api-key'] = API_key
-    # Configure the Brevo client using the provided API key
-    Email_api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
-    account_api_instance = sib_api_v3_sdk.AccountApi(sib_api_v3_sdk.ApiClient(configuration))
+
+def verify_emails(start,end):
+
+    emails = load_data(r'C:\Users\saim rao\Downloads\pod_mental_emails-UK.csv')
+    nb = cofigure_client(os.getenv('NEVERBOUNCE_KEY'),'neverbounce')
     try:
-        # Get the account information using the configured client
-        api_response = account_api_instance.get_account()
-        pprint(api_response)
+        job = nb.jobs_create(emails[start:end])
+        resp = nb.jobs_parse(job['job_id'], auto_start=False)
+        print('\u2713 job created successfully\n',job,'\n',resp)
     except Exception as e:
-        print("Exception when calling AccountApi->get_account: %s\n" % e)
-    return Email_api_instance
+        print ('\u2717 error creating job', e)
+    try:
+        resp = nb.jobs_start(job_id=job['job_id'])
+        progress = nb.jobs_status(job['job_id'])
+        print(progress)
+        while progress['job_status'] != 'complete':
+            progress = nb.jobs_status(job['job_id'])
+        print('\u2713 job completed successfully')
+    except Exception as e:
+        print('\u2717 error occurred in job completion','\n',e)
+    try:
+        jobs = nb.jobs_results(job_id=job['job_id'])
+        f = open('results.csv', mode='wb')
+        nb.jobs_download(job_id=job['job_id'], fd=f)
+        f.close()
+        verified_emails=[]
+        for job in jobs:
+            if job['verification']['result'] == 'valid':
+                verified_emails.append(job['data'])
+        print('\u2713 results fetched successfully')
+        return verified_emails
+    except Exception as e:
+        print('\u2717 error fetching results', '\n', e)
+
+
+
 
 
 '''
@@ -69,40 +76,81 @@ bcc (optional): List of email addresses and names (optional) of the recipients i
 '''
 
 
-def send_emails(subject, HTML_content, sender, to, reply_to, headers, email_api_instance, cc=None, bcc=None,
+def send_emails(to, headers,template_id=None,subject=None,HTML_content=None, sender=None,  reply_to=None, cc=None, bcc=None,
                 params=None):
+    configuration = cofigure_client(os.getenv('BREVO_KEY'),'brevo')
+    email_api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
     # Create an instance of SendSmtpEmail with the provided parameters
-    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, bcc=bcc, cc=cc, reply_to=reply_to, headers=headers,
-                                                   html_content=HTML_content, sender=sender, subject=subject)
-    try:
-        # Send the transactional email using the configured client and the SendSmtpEmail instance
-        api_response = email_api_instance.send_transac_email(send_smtp_email)
-        pprint(api_response)
-    except ApiException as e:
-        print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
+    if template_id == None:
+        send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, bcc=bcc, cc=cc, reply_to=reply_to, headers=headers,
+                                                       html_content=HTML_content, sender=sender, subject=subject,params=params)
+        try:
+            # Send the transactional email using the configured client and the SendSmtpEmail instance
+            api_response = email_api_instance.send_transac_email(send_smtp_email)
+            pprint(api_response)
+            return ('\u2713 emails sent successfully')
+        except ApiException as e:
+            print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
+            return '\u2717 error in sending Emails', e
+    else:
+        send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, bcc=bcc, cc=cc, headers=headers,template_id=template_id)
+
+        try:
+            # Send the transactional email using the configured client and the SendSmtpEmail instance
+            api_response = email_api_instance.send_transac_email(send_smtp_email)
+            pprint(api_response)
+            return ('\u2713 emails sent successfully')
+        except ApiException as e:
+            print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
+            return '\u2717 error in sending Emails', e
+
+def generate_report(limit,startdate,enddate,offset=0,email=None,event=None,tags=None,template_id=None):
+
+        configuration = cofigure_client('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', 'brevo')
+        report_api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+        if template_id == None:
+            try:
+                api_response = report_api_instance.get_email_event_report(limit=limit, offset=offset, start_date=startdate,
+                                                                   end_date=enddate, email=email, event=event, tags=tags,
+                                                                   template_id=template_id)
+                pprint(api_response)
+                return '\u2713 report generated successfully'
+            except ApiException as e:
+                print("Exception when calling SMTPApi->get_email_event_report: %s\n" % e)
+                return '\u2717 error generating report', e
+        else:
+            try:
+                api_response = report_api_instance.get_email_event_report(limit=limit, offset=offset,
+                                                                          start_date=startdate,
+                                                                          end_date=enddate,
+                                                                          template_id=template_id)
+                pprint(api_response)
+                return '\u2713 report generated successfully'
+            except ApiException as e:
+                print("Exception when calling SMTPApi->get_email_event_report: %s\n" % e)
+                return '\u2717 error generating report', e
+
+
+
+
 
 
 if __name__ == "__main__":
-    # Configure the Brevo client
-    instance = configure_Brevo_client(
-        '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')
-
+    #
     # Define the email parameters
     subject = "test mail"
     html_content = "<html><body><h1>This is test transactional email</h1><br><p>Task: </p><a href='https://app.clickup.com/t/866aa5v4h'>Clickup task</a></body></html>"
     sender = {"name": "Saim Rao", "email": "saim@sjcurve.com"}
-    to = [{"email": "hassan@sjcurve.com", "name": "Hassan"}]
+    to = [{"email": "saimrao49@gmail.com", "name": "saim"},{"email": "saimrao2408@gmail.com", "name": "saim"}]
     cc = [{"email": "example2@example2.com", "name": "Janice Doe"}]
     bcc = [{"name": "John Doe", "email": "example@example.com"}]
     reply_to = {"email": "saim@sjcurve.com", "name": "Saim Rao"}
     headers = {"Some-Custom-Name": "unique-id-1234"}
     params = {"parameter": "My param value", "subject": "New Subject"}
 
-    # Send the emails using the configured client and the email parameters
-    send_emails(subject, html_content, sender, to, reply_to, headers, instance)
+    print(verify_emails(41,44))
+    print(send_emails( to,headers,2))
+    print(generate_report(300,'2023-05-24','2023-05-24',template_id=2))
 
-    # Configure the NeverBounce client
-    configure_neverbounce_client('private_5f36cd0ca4d6864da9164f363fdad07e')
 
-    # Load data from the CSV file and convert it to a dictionary
-    data_dict = load_data(r'C:\Users\saim rao\Downloads\pod_mental_emails-UK.csv')
+
---
+++ utils.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+from pprint import pprint
+import neverbounce_sdk
+import pandas as pd
+
+# def configure_neverbounce_client(API_key):
+#     api_key = API_key
+#     # Configure the NeverBounce client using the provided API key
+#     client = neverbounce_sdk.client(api_key=api_key, timeout=30)
+#     # Get the account information using the configured client
+#     info = client.account_info()
+#     pprint(info)
+#
+#
+#
+# def configure_Brevo_client(API_key):
+#     configuration = sib_api_v3_sdk.Configuration()
+#     configuration.api_key['api-key'] = API_key
+#     # Configure the Brevo client using the provided API key
+#     Email_api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+#     account_api_instance = sib_api_v3_sdk.AccountApi(sib_api_v3_sdk.ApiClient(configuration))
+#     try:
+#         # Get the account information using the configured client
+#         api_response = account_api_instance.get_account()
+#         pprint(api_response)
+#     except Exception as e:
+#         print("Exception when calling AccountApi->get_account: %s\n" % e)
+#     return Email_api_instance
+
+def load_data(csv_path):
+    # Specify the columns to read from the CSV
+    columns = ['name', 'email']
+    # Read the CSV file and select the desired columns
+    data = pd.read_csv(csv_path, usecols=columns)
+    # Convert the DataFrame to a dictionary
+    data_dict = data.to_dict(orient='list')
+    result_lst = []
+
+    for i,j in zip(data_dict['name'],data_dict['email']):
+        temp_dict = {}
+        if type(j) != float:
+            temp_dict['name'] = i
+            temp_dict['email'] = j
+            result_lst.append(temp_dict)
+
+
+    return result_lst
+
+def cofigure_client(API_key,tool):
+    if tool == 'neverbounce':
+        api_key = API_key
+        try:
+            # Configure the NeverBounce client using the provided API key
+            client = neverbounce_sdk.client(api_key=api_key, timeout=30)
+            # Get the account information using the configured client
+            info = client.account_info()
+            print('\u2713 Client created successfully')
+            pprint(info)
+
+            return client
+        except Exception as e:
+            return ('\u2717 Error configuring client',e)
+    elif tool == 'brevo':
+
+        try:
+            configuration = sib_api_v3_sdk.Configuration()
+            configuration.api_key['api-key'] = API_key
+            # Configure the Brevo client using the provided API key
+
+            account_api_instance = sib_api_v3_sdk.AccountApi(sib_api_v3_sdk.ApiClient(configuration))
+
+            # Get the account information using the configured client
+            api_response = account_api_instance.get_account()
+            print('\u2713 Client created successfully')
+            pprint(api_response)
+            return configuration
+        except Exception as e:
+            return ('\u2717 Error configuring client','\n',e)
+    else:
+        print('Wrong arguments')
+        return 1
+
+
+
+
